da40f17108c13d56872f5d3b791a3d7d6759c887
Nikita Taranin <pro100grog@yandex.ru>
2021-07-31 22:14:21 +0200

    fix: исправил ошибки в верстке указанные ревьюером

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,28 @@
 let openedButton = document.querySelector('.profile__button-edit');
-let editForm = document.querySelector('.edit-form');
-let closedButton = editForm.querySelector('.edit-form__btn-closed');
-openedButton.addEventListener('click', function () {
-    editForm.classList.add('edit-form_opened');
-})
-closedButton.addEventListener('click', function() {
-    editForm.classList.remove('edit-form_opened');
-})
-
-let saveButton = editForm.querySelector('.edit-form__btn');
-let nameImport = editForm.querySelector('.edit-form__input-text_value_name');
+let popup = document.querySelector('.edit-form');
+let closedButton = popup.querySelector('.edit-form__btn-closed');
+let nameImport = popup.querySelector('.edit-form__input-text_value_name');
 let profileTitle = document.querySelector('.profile__title');
-let jobImport = editForm.querySelector('.edit-form__input-text_value_job');
+let jobImport = popup.querySelector('.edit-form__input-text_value_job');
 let profileSubtitle = document.querySelector('.profile__subtitle');
-editForm.addEventListener('submit', function(evt) {
+function openPopup () {
+    popup.classList.add('edit-form_opened');
+    nameImport.value = profileTitle.textContent;
+    jobImport.value = profileSubtitle.textContent;
+}
+
+function closePopup () {
+    popup.classList.remove('edit-form_opened');
+}
+
+openedButton.addEventListener('click', openPopup);
+closedButton.addEventListener('click', closePopup);
+
+function formSubmitHandler (evt) {
     evt.preventDefault();
     profileTitle.textContent = nameImport.value;
     profileSubtitle.textContent = jobImport.value;
-    editForm.classList.remove('edit-form_opened');
-})
+    closePopup();
+}
+
+popup.addEventListener('submit', formSubmitHandler);
