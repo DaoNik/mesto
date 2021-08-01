5db8ca95d8809115ea1fcdbc783e82163cf16e7e
Nikita Taranin <pro100grog@yandex.ru>
2021-08-01 11:07:54 +0200

    fix: переделал попап

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 let openedButton = document.querySelector('.profile__button-edit');
-let popup = document.querySelector('.edit-form');
-let closedButton = popup.querySelector('.edit-form__btn-closed');
+let popup = document.querySelector('.popup');
+let closedButton = popup.querySelector('.popup__btn-closed');
 let nameImport = popup.querySelector('.edit-form__input-text_value_name');
 let profileTitle = document.querySelector('.profile__title');
 let jobImport = popup.querySelector('.edit-form__input-text_value_job');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 function openPopup () {
-    popup.classList.add('edit-form_opened');
+    popup.classList.add('popup_opened');
     nameImport.value = profileTitle.textContent;
     jobImport.value = profileSubtitle.textContent;
 }
 
 function closePopup () {
-    popup.classList.remove('edit-form_opened');
+    popup.classList.remove('popup_opened');
 }
 
 openedButton.addEventListener('click', openPopup);
