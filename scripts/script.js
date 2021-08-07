1955836b4e8e7e62f1f5371a91e2737bfdbd537a
Nikita Taranin <pro100grog@yandex.ru>
2021-08-07 20:08:42 +0200

    feat: добавил 2-4 пункт ТЗ

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
 let openedButtonEdit = document.querySelector('.profile__button-edit');
 let openedButtonAdd = document.querySelector('.profile__button-add');
-const popup = document.querySelector('popup');
+const popup = document.querySelectorAll('popup');
 let popupEdit = document.querySelector('.popup_edit');
 let popupAdd = document.querySelector('.popup_add');
-let popupForm = document.querySelector('.popup__form');
-let closedButton = document.querySelector('.popup__btn-closed');
-let nameImport = document.querySelector('.popup__input-text_value_name');
+let popupEditForm = popupEdit.querySelector('.popup__form');
+let popupAddForm = popupAdd.querySelector('.popup__form')
+let closedButton = document.querySelectorAll('.popup__btn-closed');
+let nameImport = popupEdit.querySelector('.popup__input-text_value_name');
 let profileTitle = document.querySelector('.profile__title');
-let jobImport = document.querySelector('.popup__input-text_value_job');
+let jobImport = popupEdit.querySelector('.popup__input-text_value_job');
 let profileSubtitle = document.querySelector('.profile__subtitle');
+const placeImport = popupAdd.querySelector('.popup__input-text_value_place');
+const placeLinkImport = popupAdd.querySelector('.popup__input-text_value_place-link');
 const galleryCards = document.querySelector('.gallery__cards');
 const templateCard = document.getElementById('template-card').content;
 const initialCards = [
@@ -67,19 +70,40 @@ function openPopupAdd () {
     openPopup(popupAdd);
 }
 
-function closePopup (evt) {
-    const popup = evt.target.parentElement.parentElement;
+function closePopup (evt, popup) {
+  if (popup === undefined) {
+    popup = evt.target.parentElement.parentElement;
+  }
     popup.classList.remove('popup_opened');
 }
 
-function formSubmitHandler (evt) {
+function formEditSubmitHandler (evt) {
     evt.preventDefault();
     profileTitle.textContent = nameImport.value;
     profileSubtitle.textContent = jobImport.value;
-    closePopup();
+    closePopup(evt, evt.target.parentElement.parentElement);
+}
+
+function formAddSubmitHandler (evt) {
+    evt.preventDefault();
+    const galleryCard = newCard(placeImport.value, placeLinkImport.value);
+    galleryCards.prepend(galleryCard);
+    closePopup(evt, evt.target.parentElement.parentElement);
+}
+
+function clickGallery (evt) {
+    if (evt.target.classList[0] === 'gallery__card-btn') {
+      evt.target.classList.toggle('gallery__card-btn_active');
+    }
+
+    
 }
 
 openedButtonEdit.addEventListener('click', openPopupEdit);
 openedButtonAdd.addEventListener('click', openPopupAdd);
-closedButton.addEventListener('click', closePopup);
-popupForm.addEventListener('submit', formSubmitHandler);
+for (let i = 0; i < closedButton.length; i++) {
+  closedButton[i].addEventListener('click', closePopup);
+}
+popupEditForm.addEventListener('submit', formEditSubmitHandler);
+popupAddForm.addEventListener('submit', formAddSubmitHandler);
+galleryCards.addEventListener('click', clickGallery)
